654968934ca32db5ee3cfb344688ce36280c1801
manuel <manuelturlan14@gmail.com>
2022-07-02 19:41:43 -0400

    Ejercicio 2 terminado

---
+++ ejerciciojs-2.js
@@ -0,0 +1,12 @@
+let myName = 'Manuel';
+let myAge = 21;
+let imDeveloper = true;
+let dateBirth = new Date("09 June 2001");
+const favoriteBooks = {
+    book: {
+        title: "El alquimista",
+        autor: "Paulo Coehlo",
+        fecha: 1988,
+        url: 'https://www.amazon.com/El-Alquimista-Fabula-Seguir-Suenos/dp/0062511408',
+    }
+}
